2cbec94e33c94394d718b60e4580f360fc02dced
jgarms <jgarms@gmail.com>
2022-12-05 16:00:08 -0800

    Initial Commit

---
+++ WhiskeyThief/WhiskeyThiefApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiskeyThiefApp.swift
+//  WhiskeyThief
+//
+//  Created by Lisa Garms on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct WhiskeyThiefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
